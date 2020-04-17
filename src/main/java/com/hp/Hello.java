ee9aedc2a0bea2d33f8c7c80039c40a872147705
Your Name <you@example.com>
2020-04-17 16:34:24 +0800

    第一次提交

---
+++ src/main/java/com/hp/Hello.java
@@ -0,0 +1,13 @@
+package com.hp;
+
+/**
+ * @Description:
+ * @Author: Akriar
+ * @CreateDate: 2020/4/17 - 16:31
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
